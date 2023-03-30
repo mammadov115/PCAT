955894497a6abc2c3df70fa8de9cb14493b2b53e
Mammadov <mehman.mammadov182@gmail.com>
2023-03-30 17:21:30 +0400

    commit 1

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require('express');
+const ejs = require('ejs');
+
 const path = require('path');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware log 1');
-  next();
-};
+// Template Engine
+app.set('view engine', 'ejs');
+
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware log 1');
+//   next();
+// };
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger)
+// app.use(myLogger);
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'temp/index.html'))
-})
+// Routes
+
+// Index
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'views/index.html'));
+  res.render('index')
+});
+
+// About
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+
+app.get('/video-page', (req, res) => {
+  res.render('video-page')
+});
 
 
 const port = 8080;
